8811e53b96f0b3bdd588487bbd7a04fa8477c764
otabek <@otabek_249>
2024-02-22 11:15:17 +0500

    MyBookScreen part update

--- app/src/main/java/uz/developers/asaxiybooks/presenter/screen/MyBooksScreen.kt
+++ app/src/main/java/uz/developers/asaxiybooks/presenter/screen/MyBooksScreen.kt
@@ -5,9 +5,11 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
 import uz.developers.asaxiybooks.databinding.ScreenMybooksBinding
 import uz.developers.asaxiybooks.presenter.adapter.MyBookAdapter
 
+@AndroidEntryPoint
 class MyBooksScreen:Fragment() {
     private val binding by viewBinding(ScreenMybooksBinding::bind)
     private val adapter by lazy { MyBookAdapter() }
