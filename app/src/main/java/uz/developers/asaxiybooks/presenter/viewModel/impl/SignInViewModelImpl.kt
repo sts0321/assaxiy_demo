e9d378ddcc08db1efcc87b7996afd274c606a11b
Azamxoʻja Iskandarxoʻjayev <aiskandarxojayev@gmail.com>
2024-02-22 10:57:08 +0500

    inject

--- app/src/main/java/uz/developers/asaxiybooks/presenter/viewModel/impl/SignInViewModelImpl.kt
+++ app/src/main/java/uz/developers/asaxiybooks/presenter/viewModel/impl/SignInViewModelImpl.kt
@@ -3,7 +3,8 @@ package uz.developers.asaxiybooks.presenter.viewModel.impl
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import uz.developers.asaxiybooks.presenter.viewModel.SignInViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class SignInViewModelImpl : ViewModel(), SignInViewModel {
+class SignInViewModelImpl @Inject constructor() : ViewModel(), SignInViewModel {
 }
