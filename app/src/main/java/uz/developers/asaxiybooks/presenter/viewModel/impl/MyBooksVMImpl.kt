6cb9f862aaafc2103bb7ab5791be522899ceb7b2
Dio Dio <diodio10101@gmail.com>
2024-02-22 15:22:29 +0500

    salom

--- app/src/main/java/uz/developers/asaxiybooks/presenter/viewModel/impl/MyBooksVMImpl.kt
+++ app/src/main/java/uz/developers/asaxiybooks/presenter/viewModel/impl/MyBooksVMImpl.kt
@@ -1,6 +1,28 @@
 package uz.developers.asaxiybooks.presenter.viewModel.impl
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import uz.developers.asaxiybooks.data.model.MyBooksData
+import uz.developers.asaxiybooks.data.model.TypeEnum
+import uz.developers.asaxiybooks.domain.AppRepository
 import uz.developers.asaxiybooks.presenter.viewModel.MyBooksVM
+import javax.inject.Inject
 
-class MyBooksVMImpl:MyBooksVM {
+
+@HiltViewModel
+class MyBooksVMImpl @Inject constructor(
+    val appRepository: AppRepository
+):ViewModel(),MyBooksVM {
+    override fun getBooks(): Flow<Result<List<MyBooksData>>> = callbackFlow{
+        appRepository.getAllBooks(TypeEnum.PDF).onEach {
+            trySend(it)
+        }.launchIn(viewModelScope)
+        awaitClose()
+    }
 }
