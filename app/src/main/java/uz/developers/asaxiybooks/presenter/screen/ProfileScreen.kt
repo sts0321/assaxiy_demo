1b6dd44e2e560d2745cd584b30da65c9181207d4
Saidmurod Tursunaliyev <tursunaliyevsaidmurod@gmail.com>
2024-02-23 17:09:55 +0500

    profilee

--- app/src/main/java/uz/developers/asaxiybooks/presenter/screen/ProfileScreen.kt
+++ app/src/main/java/uz/developers/asaxiybooks/presenter/screen/ProfileScreen.kt
@@ -1,13 +1,16 @@
 package uz.developers.asaxiybooks.presenter.screen
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import uz.developers.asaxiybooks.R
+import uz.developers.asaxiybooks.data.model.UserData
 import uz.developers.asaxiybooks.data.sourse.Pref
 import uz.developers.asaxiybooks.data.sourse.PrefImpl
 import uz.developers.asaxiybooks.databinding.ScreenProfileBinding
@@ -27,6 +30,25 @@ class ProfileScreen : Fragment(R.layout.screen_profile) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.textEditName.text = pref.getUserInfo().firstName + " " + pref.getUserInfo().lastName
+        binding.logOut.setOnClickListener{
+
+        }
+    }
+
+    private fun dialogLogOut(){
+        val builder: AlertDialog.Builder = AlertDialog.Builder(context)
+        builder
+            .setMessage("Log Out")
+            .setTitle("Do you want Log Out? ")
+            .setPositiveButton("Log Out") { dialog, which ->
+                pref.setUserInfo(UserData("","","","",""))
+            }
+            .setNegativeButton("Cancel") { dialog, which ->
+                // Do something else.
+            }
+
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
 
     }
 
