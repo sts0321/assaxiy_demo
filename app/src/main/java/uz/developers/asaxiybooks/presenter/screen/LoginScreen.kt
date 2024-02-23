44e695e4a94c61a69963826197ce5292a3d81b2b
otabek <@otabek_249>
2024-02-23 16:02:19 +0500

    new screen xml

--- app/src/main/java/uz/developers/asaxiybooks/presenter/screen/LoginScreen.kt
+++ app/src/main/java/uz/developers/asaxiybooks/presenter/screen/LoginScreen.kt
@@ -46,7 +46,6 @@ class LoginScreen : Fragment(R.layout.screen_login) {
 
 
         binding.singUpText.setOnClickListener {
-
             findNavController().navigate(R.id.action_loginScreen_to_signUpScreen)
         }
 
